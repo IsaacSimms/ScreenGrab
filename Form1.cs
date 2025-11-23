b1d470c1c137f687be0190f1da853ad7c306b3f4
Isaac Simms <isaacsimms11@gmail.com>
2025-11-22 23:16:48 -0600

    architecture work added WinAPI calls to ActiveWindowGrab

--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,7 @@
 // Namespaces
 using System;
 using System.Windows.Forms;
-using ScreenGrab.Services; // Functionalities are split into seperate services
+using System.Drawing;
 namespace ScreenGrab
 {
     public partial class Form1 : Form
