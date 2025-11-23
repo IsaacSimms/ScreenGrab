ea4744f7e15584077cb6f1922754f4243f40eee9
Isaac Simms <isaacsimms11@gmail.com>
2025-11-22 22:42:07 -0600

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+///// Forms1 handles the US shell, tray behavior, and wires the application together /////
+
+// Namespaces
+using System;
+using System.Windows.Forms;
+using ScreenGrab.Services; // Functionalities are split into seperate services
+namespace ScreenGrab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
