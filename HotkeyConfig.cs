e306c4b75f3bb4ebec8883bd3445492629e92a4a
Isaac Simms <isaacsimms11@gmail.com>
2025-11-30 14:46:15 -0600

    started config class so that user can select their own hotkeys, then baby work up. more work needs done there

---
+++ HotkeyConfig.cs
@@ -0,0 +1,42 @@
+﻿///// handles backend of the user being able to configure hotkeys /////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenGrab
+{
+    // hotkey modifier flags
+    [Flags]
+    public enum HotkeyModifiers
+    {
+        None    = 0x0000,
+        Alt     = 0x0001,
+        Control = 0x0002,
+        Shift   = 0x0004,
+        Win     = 0x0008
+    }
+    // define hotkey structure and return hotkeys as string
+    public class HotkeyDefinition
+    {
+        public HotkeyModifiers Modifiers { get; set; }
+        public Keys            Key       { get; set; }
+
+        public override string ToString()
+        {
+            string text = "";
+            if (Modifiers.HasFlag(HotkeyModifiers.Control)) text += "Ctrl + ";
+            if (Modifiers.HasFlag(HotkeyModifiers.Alt))     text += "Alt + ";
+            if (Modifiers.HasFlag(HotkeyModifiers.Shift))   text += "Shift + ";
+            if (Modifiers.HasFlag(HotkeyModifiers.Win))     text += "Win + ";
+            text += Key.ToString();
+            return text;
+        }
+    }
+    // hotkey configuration class
+    public class HotkeyConfig
+    {
+        /// add 
+    }
+}
